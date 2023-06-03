869a678b2a0484cf202a09b7f5111b2aae1c0799
Jad Ghalayini <jad.ghalayini@protonmail.com>
2023-06-03 13:23:51 +0100

    PomEquiv approach

--- Sparky.lean
+++ Sparky.lean
@@ -3,6 +3,9 @@ import Mathlib.Order.RelIso.Basic
 import Mathlib.Order.Synonym
 import Mathlib.Data.Sigma.Order
 import Mathlib.Data.Sum.Order
+import Mathlib.Data.Finite.Defs
+import Mathlib.Data.Fintype.Card
+import Mathlib.Logic.Equiv.Defs
 
 open Classical
 
@@ -71,6 +74,101 @@ def Pom.par {L} (α β: Pom L): Pom L := {
   order := @Sum.instPartialOrderSum _ _ α.order β.order,
   action := Sum.elim α.action β.action
 }
+
+def Pom.pred {L} (α: Pom L) (p: α.carrier): Type
+  := {q: α.carrier // α.order.le q p}
+
+def Pom.finite_pred {L} (α: Pom L) (p: α.carrier): Prop
+  := Finite (α.pred p)
+
+def Pom.infinite_pred {L} (α: Pom L) (p: α.carrier): Prop
+  := Infinite (α.pred p)
+
+class Ticked (L: Type) :=
+  δ: L
+
+structure PomEquiv {L} [Ticked L] (α β: Pom L) :=
+  left_split: Pom L
+  right_split: Pom L
+  left_iso: PomIso α left_split
+  right_iso: PomIso β right_split
+  shared: Type
+  left_removed: Type
+  right_removed: Type
+  left_carrier: left_split.carrier = (shared ⊕ left_removed)
+  right_carrier: right_split.carrier = (shared ⊕ right_removed)
+  left_removed_infinite_or_delta:
+    ∀p: left_removed, 
+      let p := left_carrier.symm ▸ (Sum.inr p);
+      left_split.action p = Ticked.δ ∨ left_split.infinite_pred p 
+  right_removed_infinite_or_delta:
+    ∀p: right_removed, 
+      let p := right_carrier.symm ▸ (Sum.inr p);
+      right_split.action p = Ticked.δ ∨ right_split.infinite_pred p
+  left_infinite: Infinite α.carrier -> Infinite shared
+  right_infinite: Infinite β.carrier -> Infinite shared
+  left_nonempty: Nonempty α.carrier -> Nonempty shared
+  right_nonempty: Nonempty β.carrier -> Nonempty shared
+  shared_infinite_preserved:
+    ∀p: shared,
+      left_split.infinite_pred (left_carrier ▸ (Sum.inl p)) <->
+      right_split.infinite_pred (right_carrier ▸ (Sum.inl p))
+  shared_order_preserved:
+    ∀l r: shared,
+      left_split.order.le 
+        (left_carrier ▸ (Sum.inl l)) 
+        (left_carrier ▸ (Sum.inl r))
+      <-> right_split.order.le
+        (right_carrier ▸ (Sum.inl l))
+        (right_carrier ▸ (Sum.inl r))
+  shared_action_preserved:
+    ∀p: shared,
+      left_split.action (left_carrier ▸ (Sum.inl p)) =
+      right_split.action (right_carrier ▸ (Sum.inl p))
+
+theorem PomEquiv.left_infinite_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Infinite α.carrier <-> Infinite E.shared
+  := Iff.intro 
+    E.left_infinite 
+    (λI => by {
+      rw [E.left_iso.infinite_iff, E.left_carrier]
+      apply Sum.infinite_of_left
+    })
+
+theorem PomEquiv.right_infinite_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Infinite β.carrier <-> Infinite E.shared
+  := Iff.intro 
+    E.right_infinite     
+    (λI => by {
+      rw [E.right_iso.infinite_iff, E.right_carrier]
+      apply Sum.infinite_of_left
+    })
+
+theorem PomEquiv.left_right_infinite_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Infinite α.carrier <-> Infinite β.carrier
+  := by rw [left_infinite_iff E, right_infinite_iff E]
+
+theorem PomEquiv.left_nonempty_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Nonempty α.carrier <-> Nonempty E.shared
+  := Iff.intro 
+    E.left_nonempty 
+    (λI => by {
+      rw [Equiv.nonempty_congr E.left_iso.toEquiv, E.left_carrier]
+      simp [I]
+    })
+
+theorem PomEquiv.right_nonempty_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Nonempty β.carrier <-> Nonempty E.shared
+  := Iff.intro 
+    E.right_nonempty
+    (λI => by {
+      rw [Equiv.nonempty_congr E.right_iso.toEquiv, E.right_carrier]
+      simp [I]
+    })
+
+theorem PomEquiv.left_right_nonempty_iff {L} [Ticked L] {α β: Pom L}
+  (E: PomEquiv α β): Nonempty α.carrier <-> Nonempty β.carrier
+  := by rw [left_nonempty_iff E, right_nonempty_iff E]
 
 def SubPom {L} (P: Pom L): Type := P.carrier -> Prop
 
