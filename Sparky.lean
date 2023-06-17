eb14dfcc53e3f231a12e65b4406e1e3b5bc5fff5
Jad Ghalayini <jad.ghalayini@protonmail.com>
2023-06-17 23:17:38 +0100

    Tap

--- Sparky.lean
+++ Sparky.lean
@@ -1,16 +1,6 @@
-import Mathlib.Init.Algebra.Order
-import Mathlib.Order.RelIso.Basic
-import Mathlib.Order.Synonym
-import Mathlib.Data.Set.Finite
-import Mathlib.Data.Sigma.Order
-import Mathlib.Data.Sum.Order
-import Mathlib.Data.Finite.Defs
-import Mathlib.Data.Fintype.Card
-import Mathlib.Logic.Equiv.Defs
-import Mathlib.Logic.Relation
-
 import Sparky.Pom
 import Sparky.PomIso
 import Sparky.SubPom
 import Sparky.PomReduce
 import Sparky.PomEquiv
+import Sparky.OrderMonad
