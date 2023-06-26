66730cc4c7217e45389a8f389a1915a6c093df9e
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-06-26 16:47:51 +0100

    Tap

--- Sparky/PomEquiv/Sigma.lean
+++ Sparky/PomEquiv/Sigma.lean
@@ -11,6 +11,10 @@ def PomEquiv.sigma
     shared := Pom.sigma (λn => (E n).shared),
     reduce_left := PomReduct.sigma (λn => (E n).reduce_left),
     reduce_right := PomReduct.sigma (λn => (E n).reduce_right),
-    iso_left := sorry,
-    iso_right := sorry
+    iso_left := PomIso.trans 
+      (SubPom.sigma_iso (λn => (E n).reduce_left.shared)) 
+      (PomIso.sigma (λn => (E n).iso_left)),
+    iso_right := PomIso.trans 
+      (SubPom.sigma_iso (λn => (E n).reduce_right.shared)) 
+      (PomIso.sigma (λn => (E n).iso_right))
   }
